52f1327139ee013b7138c5c76593a569bebe9bcf
AlbertoMG <zaragozamejor@gmail.com>
2019-09-20 14:21:01 +0200

    Update HolaMundo.java

--- TutorialGitHub/src/main/java/HolaMundo.java
+++ TutorialGitHub/src/main/java/HolaMundo.java
@@ -12,5 +12,10 @@ public class HolaMundo {
     public static void main(String arg[]){
         System.out.println("Yepa que tal!!!");
         System.out.println("Probando GitHub");
+        escribe("jooooder");
+    }
+    
+    private static void escribe(String s){
+         System.out.println(s);
     }
 }
